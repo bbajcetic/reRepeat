082cfda7e4604872db729645b0ada3c1ea413975
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-01 23:25:43 -0800

    Add questions model to models.py

--- app_reRepeat/models.py
+++ app_reRepeat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    answer_text = models.CharField(max_length=200)
+    #update_date is the last time the question was answered, and based on the counter_level, will determine the next time it should be answered
+    update_date = models.DateTimeField('last answered')
+    #tags needs to be a string because tags is really a list of varying length, and database needs to have a set number of columns (also keep in mind the max length of 200 and maybe allow 10 tags of length 20 each or check length each time you add a tag)
+    tags = models.CharField(max_length=200)
+    #counter is from 1-5 inclusive, with 1 being the worst
+    counter_level = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
