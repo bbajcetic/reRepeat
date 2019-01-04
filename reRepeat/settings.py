629bb5164c5e887ae5278e108e5d29a0b18cdbba
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-04 00:00:03 -0800

    Add a reference in settings.py to the app's configuration file so that Django knows to include the app

--- reRepeat/settings.py
+++ reRepeat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_reRepeat.apps.app_reRepeatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
