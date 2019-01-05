df9ec5391bf73ea9ed82c7ab1f11e5e6ce4dab78
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-04 16:52:03 -0800

    Allow Question.tags model attribute to be blank

--- app_reRepeat/models.py
+++ app_reRepeat/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     #update_date is the last time the question was answered, and based on the counter_level, will determine the next time it should be answered
     update_date = models.DateTimeField('last answered')
     #tags needs to be a string because tags is really a list of varying length, and database needs to have a set number of columns (also keep in mind the max length of 200 and maybe allow 10 tags of length 20 each or check length each time you add a tag)
-    tags = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200, blank=True)
     #counter is from 1-5 inclusive, with 1 being the worst
     counter_level = models.IntegerField(default=1) #1-5
     periods = [1,7,21,75,240]
