b7e4def883fe96391358bffe211c2ef05384d77f
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-04 16:48:52 -0800

    Add function to Question model to check if Question is ready for next repetition

--- app_reRepeat/models.py
+++ app_reRepeat/models.py
@@ -10,8 +10,17 @@ class Question(models.Model):
     #tags needs to be a string because tags is really a list of varying length, and database needs to have a set number of columns (also keep in mind the max length of 200 and maybe allow 10 tags of length 20 each or check length each time you add a tag)
     tags = models.CharField(max_length=200)
     #counter is from 1-5 inclusive, with 1 being the worst
-    counter_level = models.IntegerField(default=1)
-
+    counter_level = models.IntegerField(default=1) #1-5
+    periods = [1,7,21,75,240]
+    #check if question will be ready to review soon:
+    #def ready_soon(self):
+    def is_ready(self):
+        """
+        Returns true if Question is ready to be reviewed, and false otherwise.
+        """
+        period = self.periods[self.counter_level-1]
+        next_review = self.update_date + datetime.timedelta(days=period)
+        return timezone.now() >= next_review
     def __str__(self):
         return self.question_text
 
