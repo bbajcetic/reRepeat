3b99a080fca4c4ab34dc3af4158b03dfe11c4cf7
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-08 13:21:41 -0800

    Add the main views and templates for those views
    Add answer, add, and edit views for answering, adding, and editing
    questions, and a show_question view for showing a question when clicked
    on. Also add templates for all of these views. In add, edit, and answer
    views, just list all the questions for now with a to the show question
    view displaying the question text.

--- app_reRepeat/urls.py
+++ app_reRepeat/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'app_reRepeat'
+
 urlpatterns = [
         path('', views.index, name="home"),
+        path('answer/', views.answer_questions, name="answer"),
+        path('add/', views.add_questions, name="add"),
+        path('edit/', views.edit_questions, name="edit"),
+        path('<int:question_id>/', views.show_question, name="question"),
 ]
--- app_reRepeat/views.py
+++ app_reRepeat/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
+##first way: fill context, load template, return Http object with the result of rendered template
+#def index(request):
+#    question_list = Question.objects.all()
+#    context = {
+#            'question_list':question_list,
+#    }
+#    template = loader.get_template('app_reRepeat/index.html')
+#    return HttpResponse(template.render(context))
+
+##second way: (no longer need to import loader and HttpResponse) render function takes the request object, template, and dictionary/context as arguments and returns HttpResponse object of the given template rendered with the given context
 def index(request):
-    return HttpResponse("reRepeat HOME")
+    return render(request, 'app_reRepeat/index.html')
+
+def answer_questions(request):
+    question_list = Question.objects.all()
+    context = {'question_list':question_list,}
+    return render(request, 'app_reRepeat/answer.html', context)
+
+def add_questions(request):
+    question_list = Question.objects.all()
+    context = {'question_list':question_list,}
+    return render(request, 'app_reRepeat/add.html', context)
+
+def edit_questions(request):
+    question_list = Question.objects.all()
+    context = {'question_list':question_list,}
+    return render(request, 'app_reRepeat/edit.html', context)
+
+def show_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question,}
+    return render(request, 'app_reRepeat/show_question.html', context)
