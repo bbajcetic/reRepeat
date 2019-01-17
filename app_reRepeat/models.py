626b02a2830aabf0a935652cc575f991ca45492a
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-16 23:06:39 -0800

    Update model question/answer attributes to TextField instead of CharField, and increase max length of answer_text

--- app_reRepeat/models.py
+++ app_reRepeat/models.py
@@ -3,8 +3,8 @@ import datetime
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    answer_text = models.CharField(max_length=200)
+    question_text = models.TextField(max_length=200)
+    answer_text = models.TextField(max_length=1000)
     #update_date is the last time the question was answered, and based on the counter_level, will determine the next time it should be answered
     update_date = models.DateTimeField('last answered')
     #tags needs to be a string because tags is really a list of varying length, and database needs to have a set number of columns (also keep in mind the max length of 200 and maybe allow 10 tags of length 20 each or check length each time you add a tag)
