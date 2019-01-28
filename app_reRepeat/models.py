3a06c78b409837f9f95d44593e3ccd2d99047510
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-01-27 17:47:07 -0800

    Increase Question model's answer_text field to 2000 characters

--- app_reRepeat/models.py
+++ app_reRepeat/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     question_text = models.TextField('Question',max_length=200)
-    answer_text = models.TextField('Answer',max_length=1000)
+    answer_text = models.TextField('Answer',max_length=2000)
     #update_date is the last time the question was answered, and based on the counter_level, will determine the next time it should be answered
     update_date = models.DateTimeField('last answered')
     #tags needs to be a string because tags is really a list of varying length, and database needs to have a set number of columns (also keep in mind the max length of 200 and maybe allow 10 tags of length 20 each or check length each time you add a tag)
